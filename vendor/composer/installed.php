fe0a9edab8e7b325be182f88abd950c079ab1330
Mohammed <94015566+mohammedebdullah@users.noreply.github.com>
2026-02-15 03:41:42 +0300

    Add vendor folder for getID3 library deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '302470dea10e1ba623b90ddbcf3998d6cf17eed1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '302470dea10e1ba623b90ddbcf3998d6cf17eed1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'james-heinrich/getid3' => array(
+            'pretty_version' => 'v1.9.24',
+            'version' => '1.9.24.0',
+            'reference' => '1e11b9b6eb468b522fe604a42a9fdc8ee759bf8a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../james-heinrich/getid3',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
